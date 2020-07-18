d5825f967347c7260876390df66ad1863f8afa9d
John Rossi <DACOOK1983@gamil.com>
2020-07-18 15:30:45 -0400

    first commit, dependencies added, first gets created and tested

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,15 +1,43 @@
 const express = require('express');
 
 // database access using knex
-const db = require('../data/db-config.js');
+const knex = require('../data/db-config.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+// router.get('/', (req, res) => {
+//     // 
+//     knex('posts')
+//     .then(posts => {
+//         res.json(posts);
+//     })
+//     .catch( err => {
+//         console.log(err);
+//         res.json(500).json({message: 'problem with db', error:err})
+//     });
+// });
 
-});
+router.get('/',  async (req, res, next) =>{
 
-router.get('/:id', (req, res) => {
+    try {
+        const posts = await knex('posts');
+        console.log(posts)
+        res.json(posts);
+    } catch (err) {
+         console.log(err);
+         res.json(500).json({ message: "problem with db", error: err });
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params;
+        const posts = await knex('posts').where(id);
+        res.json(posts)
+    } catch (err) {
+         console.log(err);
+         res.json(500).json({ message: "problem with db", error: err });
+    }
 
 });
 
